295c4a5502bfb239f36006d13f4fb5336cc8603b
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-02 02:51:43 +0530

    user and workout schema made

---
+++ backend/models/Workout.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const workoutSchema = new mongoose.Schema ({
+    exercise: {
+        type: String,
+        required: true
+    },
+    sets: {
+        type: Number,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+})
+const Workout = mongoose.model('Workout', workoutSchema)
+module.exports = Workout
