05bd2aca7d32d47612ca3d892171acf82712c394
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-02 17:25:12 +0530

    register working correctly now

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
+
 app.use(express.json())
 app.use((req,res,next) => {
     console.log(`${req.method} ${req.url}`)
     next()
 })
+
+const authRoutes = require('./routes/auth')
+app.use('/api',authRoutes)
+
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected successfully'))
