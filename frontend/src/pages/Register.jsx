37dcff6009ae993e453007740d12ae1fe56326d5
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-05 23:26:09 +0530

    register route

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,54 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 function Register() {
-    return <h1>Register Page</h1>
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const navigate = useNavigate()
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        
+        try{
+            await axios.post('http://localhost:3000/api/auth/register', {
+                name,
+                email,
+                password
+            })
+            navigate('/login')
+        } catch(err) {
+            setError(err.response?.data?.error || 'Something went wrong')
+        }
+    }
+    return (
+        <div>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Register</button>
+            </form>
+            {error && <p stype={{ color: 'red' }}>{error}</p>}
+        </div>
+    )
 }
 
 export default Register
