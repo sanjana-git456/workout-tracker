de7a48d3736d25b4ca35c0ac4ef390f62dcd41d7
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-06 01:49:36 +0530

    login frontend

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -33,13 +33,13 @@ function Register() {
                     onChange={(e) => setName(e.target.value)}
                 />
                 <input
-                    type="text"
+                    type="email"
                     placeholder="Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
                 <input
-                    type="text"
+                    type="password"
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
